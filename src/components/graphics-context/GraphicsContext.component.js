cd810a9387c0e253e18a94b259ac4c178dca0e31
Simon Adcock <simon@simonadcock.co.uk>
2020-12-08 17:13:55 +0000

    add fix for OrbitControls preventing touch events

--- src/components/graphics-context/GraphicsContext.component.js
+++ src/components/graphics-context/GraphicsContext.component.js
@@ -1,7 +1,7 @@
 import React, { useRef, useCallback } from 'react';
 import { extend, Canvas, useFrame, useThree } from 'react-three-fiber';
 import { OrthographicCamera, Html } from 'drei';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { OrbitControls } from '../../utils/OrbitControls';
 
 import { connect, ReactReduxContext, Provider } from "react-redux";
 
@@ -22,16 +22,16 @@ const CameraControls = (props) => {
   const controls = useRef();
   useFrame(() => controls.current.update());
   return (
-  <orbitControls
-    ref={controls}
-    args={[camera, domElement]} 
-    enablePan={false}
-    enableRotate={props.cameraMode === 'perspective'}
-  />);
+    <orbitControls
+      ref={controls}
+      args={[camera, domElement]}
+      enablePan={false}
+      enableRotate={props.cameraMode === 'perspective'}
+    />);
 }
 
 class GraphicsContextComponent extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props);
     this.cameraRef = React.createRef();
     this.raycasterFilter = this.raycasterFilter.bind(this);
@@ -40,47 +40,47 @@ class GraphicsContextComponent extends React.Component {
   raycasterFilter = (intersects, state) => {
     return this.props.raycasterActive ? intersects.slice(0, 1) : [];
   };
-  
+
   // repassing redux and firebase contexts since react-three-fiber is mounted in another component three.
-  render() {
+  render () {
     return (
       <ReactReduxContext.Consumer>
         {
-          ({store}) => (
+          ({ store }) => (
             <FirebaseContext.Consumer>
               {
                 (firebase) => (
-                    <Canvas
-                      className={styles.canvas}
-                      raycaster={{ filter: this.raycasterFilter }}
-                    >
-                      <Provider store={store}>
-                        <FirebaseContext.Provider value={firebase}>
-                          <CameraControls cameraMode={this.props.camera} />
-                          {
-                            this.props.camera === 'orthographic' &&
-                            <OrthographicCamera 
-                              makeDefault={true}
-                              ref={this.cameraRef}
-                              position={[0,0,1]}
-                              zoom={130}
-                            />
-                          }
-                          <ambientLight />
-                          <pointLight position={[10, 10, 10]} />
-                          <Round/>
-                          {
-                                this.props.collaboration &&
-                                <Contributors/>
-                          }
-                          <EditorLine/>
-                        </FirebaseContext.Provider>
-                      </Provider>
-                    </Canvas>
-                  )
-                }
+                  <Canvas
+                    className={styles.canvas}
+                    raycaster={{ filter: this.raycasterFilter }}
+                  >
+                    <Provider store={store}>
+                      <FirebaseContext.Provider value={firebase}>
+                        <CameraControls cameraMode={this.props.camera} />
+                        {
+                          this.props.camera === 'orthographic' &&
+                          <OrthographicCamera
+                            makeDefault={true}
+                            ref={this.cameraRef}
+                            position={[0, 0, 1]}
+                            zoom={130}
+                          />
+                        }
+                        <ambientLight />
+                        <pointLight position={[10, 10, 10]} />
+                        <Round />
+                        {
+                          this.props.collaboration &&
+                          <Contributors />
+                        }
+                        <EditorLine />
+                      </FirebaseContext.Provider>
+                    </Provider>
+                  </Canvas>
+                )
+              }
             </FirebaseContext.Consumer>
-            )
+          )
         }
       </ReactReduxContext.Consumer>
     )
