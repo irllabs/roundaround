98d15d10728bc1e0edec5a8dc3ec4816aa071f9f
Simon Adcock <simon@simonadcock.co.uk>
2021-02-16 10:23:52 +0000

    update landing page responisive design

--- src/components/landing-page/LandingPageRoute.js
+++ src/components/landing-page/LandingPageRoute.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/styles';
 import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import { connect } from "react-redux";
 import _ from 'lodash';
@@ -17,11 +18,6 @@ const styles = theme => ({
         display: 'flex'
     },
     container: {
-        display: 'flex',
-        flexDirection: 'row',
-
-        width: '100%',
-        maxWidth: '1000px'
     },
     left: {
         paddingRight: '2rem'
@@ -32,8 +28,8 @@ const styles = theme => ({
         justifyContent: 'center'
     },
     video: {
-        width: '520px',
-        height: '300px',
+        width: '100%',
+        paddingBottom: '66%',
         backgroundColor: '#303030'
     },
     getStartedButton: {
@@ -61,17 +57,17 @@ class LandingPageRoute extends Component {
         return (
             <>
                 <Container className={classes.root}>
-                    <Box className={classes.container}>
-                        <Box className={classes.left}>
+                    <Grid container className={classes.container} spacing={3}>
+                        <Grid item xs={12} sm={12} md={6}>
                             <h1>Gather around, make music, and have fun.</h1>
                             <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
                             <Button className={classes.getStartedButton} variant="contained" color="primary" disableElevation onClick={this.onGetStartedClick}>Get started</Button>
-                        </Box>
-                        <Box className={classes.right}>
+                        </Grid>
+                        <Grid item xs={12} sm={12} md={6}>
                             <Box className={classes.video}>
                             </Box>
-                        </Box>
-                    </Box>
+                        </Grid>
+                    </Grid>
                 </Container>
 
             </>
