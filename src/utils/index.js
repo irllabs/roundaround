ea18ee4c772fc1aeeff95a1c5b20b67d7aac9ca4
Muganwa <stevenmuganwa@live.com>
2021-10-20 16:21:46 +0300

    positioned lowpass and highpass at the bottom of the array. (#192)

--- src/utils/index.js
+++ src/utils/index.js
@@ -6,6 +6,12 @@ export const createRound = (userId) => {
     return getDefaultRoundData(userId)
 }
 
+export const arraymove = async (arr, fromIndex, toIndex) => {
+    var element = arr[fromIndex];
+    arr.splice(fromIndex, 1);
+    arr.splice(toIndex, 0, element);
+}
+
 export const uuid = () => {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
         // eslint-disable-next-line no-mixed-operators
