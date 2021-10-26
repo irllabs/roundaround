4eb3a71a4fdcaa540a9b7fda4af8e9e69b337fca
Muganwa <stevenmuganwa@live.com>
2021-10-26 21:15:06 +0300

    latest round always on top (#209)

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -122,8 +122,8 @@ class Header extends Component {
 			this.props.setRedirectAfterSignIn(null)
 		} else if (this.props.redirectAfterSignIn === '/rounds') {
 			// guest user, create a new round and redirect to there instead of /rounds
-			let newRound = await createRound(this.props.user.id)
-			let newRounds = [...this.props.rounds, newRound]
+			let newRound = createRound(this.props.user.id)
+			let newRounds = [newRound, ...this.props.rounds]
 			await this.context.createRound(newRound)
 			this.props.setRounds(newRounds)
 			this.props.setRedirectAfterSignIn(null)
