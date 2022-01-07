931b499090a018efa3c948b56846e78c4e75945c
Muganwa <stevenmuganwa@live.com>
2022-01-07 20:21:02 +0300

    fixed height and icons positioning issues (#249)
    * fixed height and icons positioning issues
    
    * fixed mixer title
    
    * Feature/230 new moving layer inspector from left panel to bottom middle mobile (#241)
    
    * Icons to react components
    
    * update window size
    
    * minimized layer inspector for mobi
    
    * all functionality in.
    
    * suggested improvements
    
    * Bug/244 layer inspector mobile no space btwn buttons (#248)
    
    * fix shrinking icons in the layer inspector
    
    * improved icon spacing
    
    * fixed dimensions
    
    * Feature/230 new moving layer inspector from left panel to bottom middle mobile (#241)
    
    * Icons to react components
    
    * update window size
    
    * minimized layer inspector for mobi
    
    * all functionality in.
    
    * suggested improvements
    
    * fix shrinking icons in the layer inspector
    
    * improved icon spacing
    
    * fixed dimensions
    
    * extra curly bracket
    
    * fixed height and icons positioning issues
    
    * fixed mixer title
    
    * utilise breakpoints

--- src/components/play/layer-settings/LayerListPopup.js
+++ src/components/play/layer-settings/LayerListPopup.js
@@ -5,9 +5,10 @@ import VolumeSlider from './VolumeSlider'
 import { CloseIcon } from './resources'
 
 
-const MixerPopup = ({
+const LayerListPopup = ({
     classes,
     showMixerPopup,
+    height,
     selectedInstrument,
     instrumentIcon,
     onMuteClick,
@@ -25,7 +26,7 @@ const MixerPopup = ({
             </IconButton>
             <Typography className={classes.mixerPopupHeaderText}>Mixer</Typography>
         </Box>
-        <Box className={classes.layerContainer}>
+        <Box className={classes.layerContainer} style={{ flex: 10 }}>
             {
                 round && round?.layers.map((layer, i) =>
                     <Box
@@ -38,9 +39,11 @@ const MixerPopup = ({
                         className={classes.layerSubContainer}
                     >
                         <Box className={classes.layer} style={(round.layers.length - 1) === i ? { borderBottom: 'none' } : {}}>
-                            <Box style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', flex: 4 }}>
+                            <Box style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'center', flex: 4 }}>
                                 <Box style={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center', paddingBottom: 5 }}>
-                                    <Box style={{ marginRight: 5 }}>{instrumentIcon(layer?.instrument?.sampler)}</Box>
+                                    <Box style={{ marginRight: 5 }}>
+                                        {instrumentIcon(layer?.instrument?.sampler)}
+                                    </Box>
                                     <Typography style={{ textTransform: 'capitalize', display: 'flex', alignItems: 'flex-start', lineHeight: 1 }}>
                                         {layer.instrument?.sample}
                                     </Typography>
@@ -73,4 +76,4 @@ const MixerPopup = ({
     </Box>
 )
 
-export default MixerPopup 
+export default LayerListPopup 
